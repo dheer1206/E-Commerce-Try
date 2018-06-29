0181c887e81fad68788e050bf0c09296a5621620
dheer1206 <dheergulati12@gmail.com>
2018-06-29 22:49:02 +0530

    heroku try 4

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const bodyParser = require( 'body-parser' ) ;
 app.use(express.static('public')) ;
 app.use( bodyParser.urlencoded( {extended : true} ) ) ;
  
+app.get( "/" , (req,res) => res.send("hello") ) ;
 
-
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 3000
 
 app.listen( PORT , function (req, res) {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
