884b371e430f61c2baf5e1092726bce2280bc900
dheer1206 <dheergulati12@gmail.com>
2018-06-29 23:13:01 +0530

    openshift try

--- server.js
+++ server.js
@@ -188,11 +188,11 @@ app.get( "/order" , function (req , res) {
     res.render( 'order.ejs' ) ;
 } ) ;
 
-//let port = process.env.OPENSHIFT_NODEJS_PORT || 3000
-//var ip = process.env.OPENSHIFT_NODEJS_IP || 'localhost'
+let port = process.env.OPENSHIFT_NODEJS_PORT || 3000
+let ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-const PORT = process.env.PORT || 4000
+//const PORT = process.env.PORT || 4000
 
-app.listen( PORT , function (req, res) {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen( port , ip,  function (req, res) {
+    console.log("Express server listening on port %d in %s mode", port , ip);
 } ) ;
